fece240fcd019be4c0c5b328eccb364823c12470
Sharif Khan <sharifkhan74647464@gmail.com>
2024-01-02 13:04:19 +0600

    added about me page

--- frontend/src/app/about/page.jsx
+++ frontend/src/app/about/page.jsx
@@ -1,5 +1,25 @@
+import Image from "next/image";
+import AboutMeTab from "@/components/AboutMeTab";
+
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div className="container">
+      <div className="grid grid-cols-2">
+        <div className="flex flex-col justify-center items-center pt-28 pr-20 h-full">
+          <Image
+            src="/images/sharifkhan.jpg"
+            width={400}
+            height={400}
+            alt="sharif khan"
+            className="rounded-3xl"
+          />
+        </div>
+        <div className="pt-28 pr-20">
+          <AboutMeTab />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AboutPage;
---
+++ frontend/src/components/AboutMeTab.jsx
@@ -0,0 +1,63 @@
+"use client";
+import { useState } from "react";
+
+const AboutMeTab = () => {
+  const [selectedId, setSelectedId] = useState(1);
+
+  const text = [
+    { id: 1, content: "skills" },
+    { id: 2, content: "certificate" },
+    { id: 3, content: "problem solving" },
+    { id: 4, content: "github" },
+  ];
+
+  const handleSwitch = (id) => {
+    setSelectedId(id);
+  };
+
+  return (
+    <div className="flex flex-col justify-start">
+      <ul className="flex gap-7 justify-start items-center">
+        <li
+          className={`py-2 px-4 rounded-md bg-gray-700 text-white drop-shadow-md cursor-pointer ${
+            selectedId === 1 ? "border-b-4 border-rose-400" : ""
+          }`}
+          onClick={() => handleSwitch(1)}
+        >
+          Skills
+        </li>
+        <li
+          className={`py-2 px-4 rounded-md bg-gray-700 text-white drop-shadow-md cursor-pointer ${
+            selectedId === 2 ? "border-b-4 border-rose-400" : ""
+          }`}
+          onClick={() => handleSwitch(2)}
+        >
+          Certificates
+        </li>
+        <li
+          className={`py-2 px-4 rounded-md bg-gray-700 text-white drop-shadow-md cursor-pointer ${
+            selectedId === 3 ? "border-b-4 border-rose-400" : ""
+          }`}
+          onClick={() => handleSwitch(3)}
+        >
+          Problem Solving
+        </li>
+        <li
+          className={`py-2 px-4 rounded-md bg-gray-700 text-white drop-shadow-md cursor-pointer ${
+            selectedId === 4 ? "border-b-4 border-rose-400" : ""
+          }`}
+          onClick={() => handleSwitch(4)}
+        >
+          Github
+        </li>
+      </ul>
+      {text.map((item) => {
+        <div className="mt-8 w-[511px] h-full py-6 px-4 rounded-md drop-shadow-2xl bg-slate-500 ">
+          {item.content}
+        </div>;
+      })}
+    </div>
+  );
+};
+
+export default AboutMeTab;
